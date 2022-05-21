46a00c164e44d07b03da8fa2a3aa3a873afe0b26
Ar-Raya Islam <Ar-Raya.Islam178@myhunter.cuny.edu>
2022-05-21 17:28:24 -0400

    Added sub-reducer to manage instructors

--- src/store/reducers/instructors.js
+++ src/store/reducers/instructors.js
@@ -1,10 +1,21 @@
-import * as at from "../actions/actionTypes";
+import * as at from '../actions/actionTypes';
+import course from './course';
 
 // REDUCER;
 const allInstructors = (state = [], action) => {
   switch (action.type) {
     case at.FETCH_ALL_INSTRUCTORS:
       return action.payload;
+    case at.ADD_INSTRUCTOR:
+      return [...state, action.payload]
+    case at.DELETE_INSTRUCTOR:
+      return state.filter(instructor => instructor.id!==action.payload);
+    case at.EDIT_INSTRUCTOR:
+      return state.map(instructor => { 
+        return (
+          instructor.id===action.payload.id ? action.payload : instructor
+        );
+      });
     default:
       return state;
   }
