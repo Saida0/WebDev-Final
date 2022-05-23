1fa3e7daddce683f316213b8599bedd3c46a3cfe
Saida Khamidova <saidakhamidova@gmail.com>
2022-05-23 00:52:25 -0400

    Added information for single-view course page and linked instructor

--- src/components/views/CourseView.js
+++ src/components/views/CourseView.js
@@ -1,19 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 const CourseView = (props) => {
   const { course } = props;
   return (
     <div>
       <h1>{course.title}</h1>
-      {course.instructor ? (
-        <h3>
-          {course.instructor.firstname + ' ' + course.instructor.lastname}
-        </h3>
-      ) : (
-        <h3>staff</h3>
-      )}
-      <div><Link to={'/'} > <button>Back to Home</button> </Link></div>
+      <h3>Location: {course.location}</h3>
+      <h3>Timeslot: {course.timeslot}</h3>
+      <h3>
+        Instructor:
+        {course.instructor ? (
+          <Link to={`/instructor/${course.instructor.id}`}>
+            <h3>
+              {course.instructor.firstname + ' ' + course.instructor.lastname}
+            </h3>
+          </Link>
+        ) : (
+          <h3>staff</h3>
+        )}
+      </h3>
+      <div>
+        <Link to={'/'}>
+          {' '}
+          <button>Back to Home</button>{' '}
+        </Link>
+      </div>
     </div>
-    
   );
 };
 
